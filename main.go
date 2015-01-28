752557fa3a934a763935ad782ef27267292d6174
haavardkjetil <vargo244@gmail.com>
2015-01-28 13:07:22 +0100

    update

--- main.go
+++ main.go
@@ -4,10 +4,23 @@ package networkCommunication
 import(
 "net"
 "fmt"
+"bytes"
+"encoding/gob"
+"log"
 )
 
-type 
+type Message struct {
+	Type string
+	Postition int
+	Order int
+}
 
-func receive(&message ) {
-	
+func receive(message *Message) {
+	var network bytes.Buffer
+	unpacker := gob.NewDecoder(&network)
+
+	err = unpacker.Decode(message)
+	if err != nil {
+        log.Fatal("decode error:", err)  //TODO: Fault acceptance
+
 }
