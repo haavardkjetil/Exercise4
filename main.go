3954c44cd162d58f5d2d48429b67fcda4d9c6f22
haavardkjetil <vargo244@gmail.com>
2015-01-28 16:51:49 +0100

    UPDATE

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 
-package networkCommunication
+package main
 
 import(
 "net"
 "fmt"
-"bytes"
-"encoding/gob"
+//"bytes"
+//"encoding/gob"
 "log"
 )
+
+const bcast = "129.241.187.255"
+const udpPort = "2877" 
 
 type Message struct {
 	Type string
@@ -15,18 +18,53 @@ type Message struct {
 	Order int
 }
 
-func receive(message *Message) {
-	var network bytes.Buffer
-	unpacker := gob.NewDecoder(&network)
-
-	err = unpacker.Decode(message)
+func receiveMessage(transmitChannel chan string) {
+	//Initializing
+	localAddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort( "",udpPort))
 	if err != nil {
-        log.Fatal("decode error:", err)  //TODO: Fault acceptance
-    }
+		log.Fatal( "Failed to resolve addr for :" + udpPort, err );
+	}
+
+	recieveConnection, err := net.ListenUDP("udp", localAddr)
+	if err != nil {
+		log.Fatal("UDP recv connection error on " + localAddr.String(), err)
+	}
+	
+	defer recieveConnection.Close()
+
+	receiveBuffer := make( []byte, 1600 ) // standard MTU size -- no packet should be bigger
+
+//	unpacker := gob.NewDecoder(&receiveBuffer)
+	//Initialization done
+
+
+		for i := 0; i<100; i++ {
+			len, from, err := recieveConnection.ReadFromUDP( receiveBuffer )
+			if from.String() == recieveConnection.LocalAddr().String() {
+				continue
+			}
+			if err != nil {
+				log.Fatal("Error receiving UDP packet: " + err.Error(),err )
+			}
+			transmitChannel <- string( receiveBuffer[ :len ] )
+			//		err = unpacker.Decode(&message)
+		}
+
+
+
 }
+
 
 func send(){
 	//
 }
 
-test
+
+func main(){
+	transmitChannel := make(chan string,5)
+	go receiveMessage(transmitChannel)
+	for {
+		message := <- transmitChannel
+		fmt.Println(message)
+	}
+}
